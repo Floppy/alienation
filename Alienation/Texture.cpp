662aab26e649ffc2d07d12a17ae76bb8bb0e2992
sds-james <sds-james>
2003-04-22 21:33:30 +0000

    *** empty log message ***

--- Alienation/Texture.cpp
+++ Alienation/Texture.cpp
@@ -59,22 +59,34 @@ bool CTexture::load(char *strDir, char **astrFnames, int iNoOfFiles)
 	
 				glGenTextures(1, &m_puiTexture[iCount]);
 
-				GLenum pixelformat;
-				switch (poTextureImage->format->BitsPerPixel)
-				{
-				case 24:
-					pixelformat = GL_RGB;
-					break;
-				default:
-					pixelformat = GL_RED;
-					break;
+				// Convert surface to right pixel format
+				if (poTextureImage->format->BitsPerPixel != 32) {
+					// Create pixel format
+					SDL_PixelFormat fmt;
+					fmt.palette = NULL;
+					fmt.BitsPerPixel = 32;
+					#if SDL_BYTEORDER == 1234
+				    fmt.Rmask=0x000000ff;
+					fmt.Gmask=0x0000ff00;
+					fmt.Bmask=0x00ff0000;
+					fmt.Amask=0xff000000;
+					#else
+				    fmt.Rmask=0xff000000;
+					fmt.Gmask=0x00ff0000;
+					fmt.Bmask=0x0000ff00;
+					fmt.Amask=0x000000ff;
+					#endif
+					// Convert
+					SDL_Surface * tmp = poTextureImage;
+					poTextureImage = SDL_ConvertSurface(tmp, &fmt, SDL_HWSURFACE);
+					SDL_FreeSurface(tmp);
 				}
 
 				// Create Linear Filtered Texture
 				glBindTexture(GL_TEXTURE_2D, m_puiTexture[iCount]);
 				glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
 				glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, poTextureImage->w, poTextureImage->h, 0, pixelformat, GL_UNSIGNED_BYTE, poTextureImage->pixels);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, poTextureImage->w, poTextureImage->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, poTextureImage->pixels);
 			}
 		}
 		// Free the SDL surface
