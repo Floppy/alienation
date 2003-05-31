836c6ccaf78dce843bf60661230a0cb93c2ea66c
sds-james <sds-james>
2003-05-31 18:31:00 +0000

    added TextureManager files

---
+++ Alienation/3D/TextureManager.cpp
@@ -0,0 +1,78 @@
+// Texture.cpp: implementation of the CTexture class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "3D/TextureManager.h"
+#include <GL/gl.h>
+
+CTextureManager g_oTextureManager;
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CTextureManager::CTextureManager()
+{
+}
+
+CTextureManager::~CTextureManager()
+{
+   // Dump all textures
+   for (std::map<unsigned int,CTexture*>::iterator it(m_hTextures.begin()); it != m_hTextures.end(); it++) {
+      delete it->second;
+   }
+}
+
+unsigned int CTextureManager::load(const char* strFilename)
+{   
+   // Create new texture
+   CTexture* pTexture = new CTexture;
+   // Load
+   if (pTexture->load(strFilename)) 
+   {
+      unsigned int uiTexture = pTexture->ID();
+      // Add to hashes
+      m_hTextures[uiTexture] = pTexture;
+      m_hReferences[uiTexture] = 1;
+      // Done
+      return uiTexture;
+   }
+   else 
+   {
+      // Drop created texture
+      delete pTexture;
+      // Done
+      return 0;
+   }
+}
+
+void CTextureManager::addReference(unsigned int uiTexture) 
+{
+   // Increment reference count
+   ++(m_hReferences[uiTexture]);
+}
+
+void CTextureManager::removeReference(unsigned int uiTexture) 
+{
+   if (--(m_hReferences[uiTexture]) == 0) {
+      // Delete texture
+      delete m_hTextures[uiTexture];
+      // Dump map entries
+      m_hTextures.erase(uiTexture);
+      m_hReferences.erase(uiTexture);
+   }   
+}
+
+bool CTextureManager::activate(unsigned int uiTexture) const
+{
+   if (valid(uiTexture)) {
+      glBindTexture(GL_TEXTURE_2D, uiTexture);
+      return true;
+   }
+   return false;
+}
+
+bool CTextureManager::valid(unsigned int uiTexture) const 
+{
+   return true;//glTexValid(uiTexture);
+}
---
+++ Alienation/3D/TextureManager.h
@@ -0,0 +1,96 @@
+// Texture.h: interface for the CTexture class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef TEXTURE_MANAGER_H
+#define TEXTURE_MANAGER_H
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "config.h"
+#include <map>
+#include "3D/Texture.h"
+
+/**
+ * Texture manager class.
+ * Deals with all texture objects, including file loading, rendering and ownership of the data.
+ * It does this by having one single global instance of the class, g_oTextureManager, and keeping 
+ * reference counts for each texture it owns. If only a single class is using a texture, then the
+ * reference count can be left at 1, it's default on load. If the texture ID is used in more than 
+ * object, each object after the first should call addReference() on creation, and removeReference()
+ * on destruction.
+ */
+class CTextureManager
+{
+ public:
+   
+   /**
+    * Constructor.
+    */
+   CTextureManager();
+
+   /**
+    * Destructor.
+    * Destroys all loaded textures.
+    */
+   ~CTextureManager();
+   
+   /**
+    * Load a texture from file.
+    * The loaded texture will have a reference count of 1. 
+    * You don't need to add a reference until you use the texture ID for more than one object.
+    * @param strFilename The texture filename to load.
+    * @return The texture ID that can be used to access the loaded texture.
+    */
+   unsigned int load(const char* strFilename);
+
+   /**
+    * Add a reference to the specified texture.
+    * @param uiTexture The texture ID to add the reference to.
+    */
+   void addReference(unsigned int uiTexture);
+
+   /**
+    * Remove a reference from the specified texture.
+    * A texture will be deleted when all references are removed.
+    * @param uiTexture The texture ID to remove the reference from.
+    */
+   void removeReference(unsigned int uiTexture);
+   
+   /**
+    * Activate a texture for rendering.
+    * @param uiTexture The texture ID to activate.
+    * @return true if the texture was activated OK.
+    */
+   bool activate(unsigned int uiTexture) const;
+   
+   /**
+    * Can the texture be rendered?
+    * @param uiTexture The texture ID to check.
+    * @return true if the texture is ready to render.
+    */
+   bool valid(unsigned int uiTexture) const;   
+
+ protected:
+   
+   /**
+    * Texture pointers.
+    */
+   std::map<unsigned int, CTexture*> m_hTextures;
+   
+   /**
+    * Reference counts.
+    */
+   std::map<unsigned int, unsigned int> m_hReferences;
+
+};
+
+/**
+ * A global instance of the CTextureManager class. 
+ * All texture access should be done through this object.
+ */
+extern CTextureManager g_oTextureManager;
+
+#endif // TEXTURE_MANAGER_H
