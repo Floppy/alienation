6d5a47f9bc8c9a61c9a1e351d34d437c75469d89
sds-james <sds-james>
2003-07-27 10:22:35 +0000

    added NSDSound namespace

--- Alienation/Sound/SoundManager.cpp
+++ Alienation/Sound/SoundManager.cpp
@@ -1,145 +1,151 @@
 #include "Sound/SoundManager.h"
 #include "config.h"
 
-CSoundManager g_oSoundManager;
+namespace NSDSound {
 
-CSoundManager::CSoundManager() :
-   m_bReady(false),
-   m_pMusic(NULL),
-   m_vecPosition(0,0,0),
-   m_vecUp(0,1,0),
-   m_vecRight(1,0,0)
-{
-   if (!Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,1024)) {
-      m_bReady = true;
-      // allocate 16 mixing channels
-      Mix_AllocateChannels(16);
+   using namespace NSDMath;
+
+   CSoundManager g_oSoundManager;
+
+   CSoundManager::CSoundManager() :
+      m_bReady(false),
+      m_pMusic(NULL),
+      m_vecPosition(0,0,0),
+      m_vecUp(0,1,0),
+      m_vecRight(1,0,0)
+   {
+      if (!Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,1024)) {
+         m_bReady = true;
+         // allocate 16 mixing channels
+         Mix_AllocateChannels(16);
+      }
+
    }
 
-}
-
-CSoundManager::~CSoundManager()
-{
-   if (m_bReady)
-      // Stop playing
-      Mix_HaltChannel(-1);
-   // Free music file
-   if (m_pMusic) 
-      Mix_FreeMusic(m_pMusic);
-   // Dump all loaded samples
-   for (std::vector<Mix_Chunk*>::iterator it(m_oSamples.begin()); it != m_oSamples.end(); it++) {
-      Mix_FreeChunk(*it);
+   CSoundManager::~CSoundManager()
+   {
+      if (m_bReady)
+         // Stop playing
+         Mix_HaltChannel(-1);
+      // Free music file
+      if (m_pMusic) 
+         Mix_FreeMusic(m_pMusic);
+      // Dump all loaded samples
+      for (std::vector<Mix_Chunk*>::iterator it(m_oSamples.begin()); it != m_oSamples.end(); it++) {
+         Mix_FreeChunk(*it);
+      }
+      // Close audio device
+      Mix_CloseAudio();
    }
-   // Close audio device
-   Mix_CloseAudio();
-}
 
-bool CSoundManager::playMusicFile(const char* strFilename) 
-{
-   if (m_bReady) {
+   bool CSoundManager::playMusicFile(const char* strFilename) 
+   {
+      if (m_bReady) {
+         // Create filename
+         char strFile[256];
+         strcpy(strFile, "./Data/Audio/");
+         strcat(strFile, strFilename);   
+         // Load file
+         m_pMusic = Mix_LoadMUS(strFile);
+         // Play
+         if (m_pMusic) {
+            Mix_PlayMusic(m_pMusic,-1);
+            return true;
+         }
+      }
+      return false;
+   }
+
+   void CSoundManager::setVolume(float fVolume, int iChannel) 
+   {
+      if (m_bReady) {
+         Mix_Volume(iChannel,static_cast<int>(MIX_MAX_VOLUME*fVolume));
+      }
+   }
+
+   float CSoundManager::getVolume(int iChannel)
+   {
+      if (m_bReady) {
+         return Mix_Volume(iChannel,-1);
+      }
+      else return 0;
+   }
+
+   int CSoundManager::load(const char* strFilename)
+   {
+      int id(-1);
       // Create filename
       char strFile[256];
       strcpy(strFile, "./Data/Audio/");
       strcat(strFile, strFilename);   
-      // Load file
-      m_pMusic = Mix_LoadMUS(strFile);
-      // Play
-      if (m_pMusic) {
-         Mix_PlayMusic(m_pMusic,-1);
-         return true;
+      // Load from file
+      Mix_Chunk* pSample = Mix_LoadWAV(strFile);
+      if (pSample) {
+         // Get ID
+         id = m_oSamples.size();
+         // Store sample
+         m_oSamples.push_back(pSample);
+      }
+      // Done
+      return id;
+   }
+
+   int CSoundManager::play(int iSample, int iNumRepeats, int iFadeIn, int iCutoff) 
+   {
+      if (m_bReady && iSample >= 0) {
+         if (iFadeIn) {
+            return Mix_FadeInChannelTimed(-1, m_oSamples[iSample], iNumRepeats, iFadeIn, iCutoff);
+         }
+         else {
+            return Mix_PlayChannelTimed(-1, m_oSamples[iSample], iNumRepeats, iCutoff);
+         }
+      }
+      return -1;
+   }
+
+   void CSoundManager::stop(int iChannel, int iFadeOut) 
+   {
+      if (m_bReady) {
+         if (iFadeOut) {
+            Mix_FadeOutChannel(iChannel, iFadeOut);
+         }
+         else {
+            Mix_HaltChannel(iChannel);
+         }
       }
    }
-   return false;
-}
 
-void CSoundManager::setVolume(float fVolume, int iChannel) 
-{
-   if (m_bReady) {
-      Mix_Volume(iChannel,static_cast<int>(MIX_MAX_VOLUME*fVolume));
+   void CSoundManager::setPlaybackLocation(CVector3 vecPosition, CVector3 vecUp, CVector3 vecRight)
+   {
+      // Store arguments
+      m_vecPosition = vecPosition; 
+      m_vecUp = vecUp;
+      m_vecRight = vecRight;
+      // Normalise vectors, just in case
+      m_vecUp.normalise();
+      m_vecRight.normalise();
    }
-}
 
-float CSoundManager::getVolume(int iChannel)
-{
-   if (m_bReady) {
-      return Mix_Volume(iChannel,-1);
-   }
-   else return 0;
-}
-
-int CSoundManager::load(const char* strFilename)
-{
-   int id(-1);
-   // Create filename
-   char strFile[256];
-   strcpy(strFile, "./Data/Audio/");
-   strcat(strFile, strFilename);   
-   // Load from file
-   Mix_Chunk* pSample = Mix_LoadWAV(strFile);
-   if (pSample) {
-      // Get ID
-      id = m_oSamples.size();
-      // Store sample
-      m_oSamples.push_back(pSample);
-   }
-   // Done
-   return id;
-}
-
-int CSoundManager::play(int iSample, int iNumRepeats, int iFadeIn, int iCutoff) 
-{
-   if (m_bReady && iSample >= 0) {
-      if (iFadeIn) {
-         return Mix_FadeInChannelTimed(-1, m_oSamples[iSample], iNumRepeats, iFadeIn, iCutoff);
+   bool CSoundManager::setChannelLocation(int iChannel, CVector3 vecPosition) 
+   {
+      if (m_bReady) {
+         // Calculate direction vector
+         CVector3 vecDirection = m_vecPosition - vecPosition;
+         // Calculate distance
+         float fLength = vecDirection.length();
+         unsigned char cDistance = (fLength<500) ? (fLength/500) * 255 : 255;
+         // Normalise direction vector
+         vecDirection /= fLength;
+         // Calculate which side the sound is
+         int iAngle = (vecDirection.dot(m_vecRight) > 0) ? -1 : 1;
+         // Take cross product of sound vector and local up vector
+         vecDirection = m_vecUp.cross(vecDirection);
+         // Calculate angle between result and local right vector
+         iAngle *= RAD_TO_DEG(vecDirection.angle(m_vecRight));
+         // Set channel position
+         return Mix_SetPosition(iChannel,iAngle,cDistance);
       }
-      else {
-         return Mix_PlayChannelTimed(-1, m_oSamples[iSample], iNumRepeats, iCutoff);
-      }
+      else return false;
    }
-   return -1;
-}
 
-void CSoundManager::stop(int iChannel, int iFadeOut) 
-{
-   if (m_bReady) {
-      if (iFadeOut) {
-         Mix_FadeOutChannel(iChannel, iFadeOut);
-      }
-      else {
-         Mix_HaltChannel(iChannel);
-      }
-   }
-}
-
-void CSoundManager::setPlaybackLocation(CVector3 vecPosition, CVector3 vecUp, CVector3 vecRight)
-{
-   // Store arguments
-   m_vecPosition = vecPosition; 
-   m_vecUp = vecUp;
-   m_vecRight = vecRight;
-   // Normalise vectors, just in case
-   m_vecUp.normalise();
-   m_vecRight.normalise();
-}
-
-bool CSoundManager::setChannelLocation(int iChannel, CVector3 vecPosition) 
-{
-   if (m_bReady) {
-      // Calculate direction vector
-      CVector3 vecDirection = m_vecPosition - vecPosition;
-      // Calculate distance
-      float fLength = vecDirection.length();
-      unsigned char cDistance = (fLength<500) ? (fLength/500) * 255 : 255;
-      // Normalise direction vector
-      vecDirection /= fLength;
-      // Calculate which side the sound is
-      int iAngle = (vecDirection.dot(m_vecRight) > 0) ? -1 : 1;
-      // Take cross product of sound vector and local up vector
-      vecDirection = m_vecUp.cross(vecDirection);
-      // Calculate angle between result and local right vector
-      iAngle *= RAD_TO_DEG(vecDirection.angle(m_vecRight));
-      // Set channel position
-      return Mix_SetPosition(iChannel,iAngle,cDistance);
-   }
-   else return false;
 }
--- Alienation/Sound/SoundManager.h
+++ Alienation/Sound/SoundManager.h
@@ -5,134 +5,138 @@
 #include "Math/Vector.h"
 #include <vector>
 
-using namespace std;
+namespace NSDSound {
 
-/**
- * Sound Manager.
- * Looks after the audio subsystem and handles playing of audio content.
- */
-class CSoundManager {
-   
- public:
-   
-   /**
-    * Constructor.
-    * Initialises the sound subsystem.
-    */
-   CSoundManager();
+   using namespace std;
+   using namespace NSDMath;
 
    /**
-    * Destructor.
-    * Shuts down the sound subsystem.
+    * Sound Manager.
+    * Looks after the audio subsystem and handles playing of audio content.
     */
-   ~CSoundManager();
+   class CSoundManager {
+   
+   public:
+   
+      /**
+       * Constructor.
+       * Initialises the sound subsystem.
+       */
+      CSoundManager();
+
+      /**
+       * Destructor.
+       * Shuts down the sound subsystem.
+       */
+      ~CSoundManager();
     
-   /**
-    * Is the sound system ready to go?
-    * @return true if the sound subsystem has been initialised OK.
-    */
-   bool ready() {return m_bReady;}
+      /**
+       * Is the sound system ready to go?
+       * @return true if the sound subsystem has been initialised OK.
+       */
+      bool ready() {return m_bReady;}
     
-   /**
-    * Play some music from file.
-    * Will play the specified file on a continuous loop.
-    * @param strFilename The filename to play.
-    * @return true if the file loaded OK.
-    */
-   bool playMusicFile(const char* strFilename);
+      /**
+       * Play some music from file.
+       * Will play the specified file on a continuous loop.
+       * @param strFilename The filename to play.
+       * @return true if the file loaded OK.
+       */
+      bool playMusicFile(const char* strFilename);
 
-   /**
-    * Set channel volume.
-    * @param iChannel The channel to change the volume on, obtained from play(). If -1, all channels are changed.
-    * @param fVolume A float between 0 (lowest volume) and 1 (highest volume).
-    */
-   void setVolume(float fVolume, int iChannel = -1);
+      /**
+       * Set channel volume.
+       * @param iChannel The channel to change the volume on, obtained from play(). If -1, all channels are changed.
+       * @param fVolume A float between 0 (lowest volume) and 1 (highest volume).
+       */
+      void setVolume(float fVolume, int iChannel = -1);
 
-   /**
-    * Get channel volume.
-    * @param iChannel The channel to get the volume from, obtained from play(). If -1, the average volume is returned.
-    * @return A float between 0 (lowest volume) and 1 (highest volume).
-    */
-   float getVolume(int iChannel = -1);
+      /**
+       * Get channel volume.
+       * @param iChannel The channel to get the volume from, obtained from play(). If -1, the average volume is returned.
+       * @return A float between 0 (lowest volume) and 1 (highest volume).
+       */
+      float getVolume(int iChannel = -1);
 
-   /**
-    * Load a sample from disk
-    * @return The sample ID. If the file could not be loaded, -1 is returned.
-    */
-   int load(const char* strFilename);
+      /**
+       * Load a sample from disk
+       * @return The sample ID. If the file could not be loaded, -1 is returned.
+       */
+      int load(const char* strFilename);
 
-   /**
-    * Play a sample
-    * @param iSample The sample ID to play.
-    * @param iNumRepeats How many times to repeat the sample. A value of 1 will play the sample twice. A value of -1 will play forever until stopped.
-    * @param iFadeIn The sample will fade in over this many milliseconds.
-    * @param iCutoff Playback will be stopped after this many milliseconds.
-    * @return A channel ID which can be used to stop the sample playing or change volume.
-    */
-   int play(int iSample, int iNumRepeats = 0, int iFadeIn = 0, int iCutoff = -1);
+      /**
+       * Play a sample
+       * @param iSample The sample ID to play.
+       * @param iNumRepeats How many times to repeat the sample. A value of 1 will play the sample twice. A value of -1 will play forever until stopped.
+       * @param iFadeIn The sample will fade in over this many milliseconds.
+       * @param iCutoff Playback will be stopped after this many milliseconds.
+       * @return A channel ID which can be used to stop the sample playing or change volume.
+       */
+      int play(int iSample, int iNumRepeats = 0, int iFadeIn = 0, int iCutoff = -1);
 
-   /**
-    * Stop sample playback.
-    * @param iChannel The channel ID to stop playing, obtained from play().
-    * @param iFadeOut Fade out over this many milliseconds.
-    */
-   void stop(int iChannel, int iFadeOut = 0);
+      /**
+       * Stop sample playback.
+       * @param iChannel The channel ID to stop playing, obtained from play().
+       * @param iFadeOut Fade out over this many milliseconds.
+       */
+      void stop(int iChannel, int iFadeOut = 0);
 
-   /**
-    * Set playback location.
-    * @param vecPosition The position of the final sound output.
-    * @param vecUp The local +Y vector at the sound output location.
-    * @param vecRight The local +X vector at the sound output location.
-    */
-   void setPlaybackLocation(CVector3 vecPosition, CVector3 vecUp, CVector3 vecRight);
+      /**
+       * Set playback location.
+       * @param vecPosition The position of the final sound output.
+       * @param vecUp The local +Y vector at the sound output location.
+       * @param vecRight The local +X vector at the sound output location.
+       */
+      void setPlaybackLocation(CVector3 vecPosition, CVector3 vecUp, CVector3 vecRight);
 
-   /**
-    * Set 3D location for a particular channel.
-    * @param iChannel The channel ID to spatialise, obtained from play().
-    * @param vecPosition The 3D position that the channel should come from.
-    * @return false if an error occurred.
-    * @bug Does not perform angle calculations yet, as I haven't bothered to work the maths out.
-    */
-   bool setChannelLocation(int iChannel, CVector3 vecPosition);
+      /**
+       * Set 3D location for a particular channel.
+       * @param iChannel The channel ID to spatialise, obtained from play().
+       * @param vecPosition The 3D position that the channel should come from.
+       * @return false if an error occurred.
+       * @bug Does not perform angle calculations yet, as I haven't bothered to work the maths out.
+       */
+      bool setChannelLocation(int iChannel, CVector3 vecPosition);
 
- protected:
+   protected:
    
-   /**
-    * Is the sound system ready to go?
-    */
-   bool m_bReady;
+      /**
+       * Is the sound system ready to go?
+       */
+      bool m_bReady;
+
+      /**
+       * An array of samples
+       */
+      vector<Mix_Chunk*> m_oSamples;
+
+      /**
+       * Background music
+       */
+      Mix_Music* m_pMusic;
+
+      /**
+       * Playback location
+       */
+      CVector3 m_vecPosition;
+
+      /**
+       * Playback location - up vector.
+       */
+      CVector3 m_vecUp;
+
+      /**
+       * Playback location - right vector.
+       */
+      CVector3 m_vecRight;
+
+   };
 
    /**
-    * An array of samples
+    * A global instance of the CSoundManager class. 
+    * All sound access should be done through this object.
     */
-   vector<Mix_Chunk*> m_oSamples;
+   extern CSoundManager g_oSoundManager;
 
-   /**
-    * Background music
-    */
-   Mix_Music* m_pMusic;
-
-   /**
-    * Playback location
-    */
-   CVector3 m_vecPosition;
-
-   /**
-    * Playback location - up vector.
-    */
-   CVector3 m_vecUp;
-
-   /**
-    * Playback location - right vector.
-    */
-   CVector3 m_vecRight;
-
-};
-
-/**
- * A global instance of the CSoundManager class. 
- * All sound access should be done through this object.
- */
-extern CSoundManager g_oSoundManager;
-
+}
 #endif
