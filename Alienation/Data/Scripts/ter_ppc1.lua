1f08fc08f572ee873d5fffc6f446a985086d74e3
sds-james <sds-james>
2003-08-30 11:48:51 +0000

    added ppc script

---
+++ Alienation/Data/Scripts/ter_ppc1.lua
@@ -0,0 +1,16 @@
+-- ter_ppc1.lua
+-- Configuration file for the Terran Purple Particle Cannon Mk.1
+
+weapon = {
+	uname   	= "ter_ppc1",
+	race    	= "terran",
+	fullname	= "PPC (Purple Particle Cannon) Mk.1",
+	class		= "projectile",
+	firerate	= 0.5,
+	sound		= "weapon.wav",
+	colour 		= {1, 0.2, 0.4, 1},
+	texture		= "ball.png",
+	size		= 1,
+	lifetime	= 7,
+	speed		= 235
+}
