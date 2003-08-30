6868f78e9b1d822594b29d96e1fef085ec43daea
sds-james <sds-james>
2003-08-30 11:05:23 +0000

    added lua scripts and dependencies

---
+++ Alienation/Data/Scripts/ter_fighter1.lua
@@ -0,0 +1,16 @@
+-- ter_fighter1.lua
+-- Configuration file for the Terran Mk.1 Fighter
+
+ship = {
+	uname   	= "ter_fighter1",
+	race    	= "terran",
+	fullname	= "Fighter Mk.1",
+	model   	= "fighter.3ds",
+	pitch   	= 80,
+	yaw 		= 50,
+	roll 		= 90,
+	thrust		= 10000,
+	trails		= {
+		{x= 0, y=0, z=5}
+	}
+}
---
+++ Alienation/Data/Scripts/ter_shuttle1.lua
@@ -0,0 +1,17 @@
+-- ter_shuttle1.lua
+-- Configuration file for the Terran Mk.1 Shuttle
+
+ship = {
+	uname   	= "ter_shuttle1",
+	race    	= "terran",
+	fullname	= "Shuttle Mk.1",
+	model   	= "shuttle.3ds",
+	pitch   	= 40,
+	yaw 		= 25,
+	roll 		= 45,
+	thrust		= 10000,
+	trails		= {
+		{x= 1.6, y=-0.25, z=3.75},
+		{x=-1.6, y=-0.25, z=3.75}
+	}
+}
