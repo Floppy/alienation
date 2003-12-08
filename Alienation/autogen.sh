0cf5dc3cec90d55d73aaf3998c2d1450a0df3de4
sds-james <sds-james>
2003-12-08 23:48:39 +0000

    added some comments

--- Alienation/autogen.sh
+++ Alienation/autogen.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo ---- Running aclocal
 aclocal
+echo ---- Running autoconf
 autoconf
+echo ---- Running automake
 automake -a
-echo Now run "./configure" followed by "make"
+echo ---- Now run "./configure" followed by "make"
 
