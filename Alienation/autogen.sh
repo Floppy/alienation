1ce320c8872823d4d269a6808c31d33d137b7221
sds-james <sds-james>
2004-01-21 22:22:58 +0000

    use libtool, and move headers to separate line in makefiles

--- Alienation/autogen.sh
+++ Alienation/autogen.sh
@@ -4,6 +4,8 @@ echo ---- Running aclocal
 aclocal
 echo ---- Running autoconf
 autoconf
+echo ---- Running libtoolize
+libtoolize
 echo ---- Running automake
 automake -a
 echo ---- Now run "./configure" followed by "make"
